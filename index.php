b44c6f1d2fa24c719810ebbc12bbd13e86c9d2bd
BarrioTropical <barriotropical@gmail.com>
2022-11-30 19:07:25 +0100

    first step
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
 
-$tasks = [
-    'fare Html',
-    'link VueJS tramite cdns',
-    'import axios',
-    'function PHP',
-    'sistemare css'
-];
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -19,22 +9,20 @@ $tasks = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    
     <title>Php Todo List Json</title>
+    <script src="https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <link rel="stylesheet" href="./css/style.css">    
 </head>
 
 <body>
 
-    <form action="newTodo.php" method="post">
-        <input type="text" name="todo_name" placeholder="Enter your to do">
-        <button>New To do </button>
+    <div id="app">
 
+    </div>
 
-    </form>
-
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-    <script src="./script/app.js"></script>
+    <script src="./script/main.js"></script>
 </body>
 
 </html>
---
+++ script/main.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            //hola: "hola",
+            todoList: [],
+            apiUrl: './server.php',
+        }
+    },
+    methods: {
+        getTodo(){
+        axios.get(this.apiUrl).then((response) => {
+            console.log(response.data);
+        })
+    }
+},
+    mounted() {
+        this.getTodo();
+    },
+}).mount('#app');
