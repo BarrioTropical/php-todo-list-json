19c932bf1cd10d47bb3cc2007eeaf01b864404d1
BarrioTropical <barriotropical@gmail.com>
2022-11-30 20:04:46 +0100

    print html json
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

--- index.php
+++ index.php
@@ -9,15 +9,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
     <title>Php Todo List Json</title>
-    <script src="https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <link rel="stylesheet" href="./css/style.css">    
 </head>
 
 <body>
-
     <div id="app">
         <h2>To do List</h2>
         <ul>
--- server.php
+++ server.php
@@ -2,11 +2,12 @@
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-Requested-With");
 
+$file_url = './todo.json';
 $file_text = file_get_contents('./todo.json');
 $todo_list = json_decode($file_text);
 
 
 header('Content-Type: application/json');
-echo $file_text;
-echo json_decode($todo_list);
+//echo $file_text;
+echo json_encode($todo_list);
 ?>
