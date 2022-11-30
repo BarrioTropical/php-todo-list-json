1c0b70f2b4dcb026a5f8e00e24f06b08b531d09b
BarrioTropical <barriotropical@gmail.com>
2022-11-30 16:56:13 +0100

    creazione form
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+$tasks = [
+    'fare Html',
+    'link VueJS tramite cdns',
+    'import axios',
+    'function PHP',
+    'sistemare css'
+];
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -9,11 +19,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Php Todo List Json</title>
 </head>
 
 <body>
 
+    <form action="" method="post">
+        <input type="text" name="todo_name" placeholder="Enter your to do">
+        <button>New To do </button>
+
+
+    </form>
+
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="./script/app.js"></script>
 </body>
 
 </html>
